1fcae12d152d32d95871531e3689b536d437edf9
Dennya <dennya@macbook-pro-dennya.local>
2016-05-28 00:36:02 +0300

    Initial Commit

---
+++ GitUse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUse
+//
+//  Created by  Dennya on 28.05.16.
+//  Copyright © 2016  Dennya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
